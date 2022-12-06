12e24d2d9f1643160c6aa4fb7ce1c2b34027cea9
Googly-Boogly <mainalextaylor@gmail.com>
2022-12-06 10:17:53 -0600

    Add files via upload

---
+++ app/run.ps1
@@ -0,0 +1,7 @@
+
+
+cd "C:\Users\maina\OneDrive\Desktop\Coding\Projects\Youtube_to_mp3"
+Start-Sleep -Seconds 0.5
+./env\Scripts\activate.ps1
+cd "C:\Users\maina\OneDrive\Desktop\Coding\Projects\Youtube_to_mp3\app"
+python run.py
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+source "./env/bin/activate"
+cd app
+python -u run.py
